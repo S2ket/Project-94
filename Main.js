4be9ff20e43e828926b04577bed3e17e96a3c686
S2ket <124378050+S2ket@users.noreply.github.com>
2023-02-05 16:06:55 +0530

    Delete Main.js

--- Main.js
+++
@@ -1,9 +0,0 @@
-function login(){
-    var username = document.getElementById("username").value;
-    if(username == ""){
-        alert("Please write a username");
-    }else{
-        localStorage.setItem("USERNAME", username);
-        window.location = "Room.html";
-    }
-}
